c9606567ccd1e2c972f2f658de1ed582ddb2c486
SiriYeung <119325339+SiriYeung@users.noreply.github.com>
2025-11-14 13:49:19 +0000

    put more in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import About from "./components/About";
+import Gallery from "./components/Gallery";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <About />
+      <Hero />
+      <Services />
+      <Gallery />
     </div>
   );
 }
